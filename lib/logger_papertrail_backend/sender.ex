f83894de652f66d832132ab07ea5325ef028463f
Lars Krantz <lars.krantz@alaz.se>
2015-10-01 14:23:15 +0200

    Made sender a genserver

--- lib/logger_papertrail_backend/sender.ex
+++ lib/logger_papertrail_backend/sender.ex
@@ -1,8 +1,44 @@
 defmodule LoggerPapertrailBackend.Sender do
-  def send(message, host, port) do
-    {:ok, address} = :inet.getaddr(String.to_char_list(host), :inet)
-    {:ok, socket} = :gen_udp.open(0)
-    :gen_udp.send(socket, address, port, message)
-    :gen_udp.close(socket)
+  use GenServer
+
+  @ip_udpdate_interval 60000
+  def init({host,port}) do
+    address = :inet.getaddr(String.to_char_list(host), :inet)
+    case address do
+      {:ok, ip} ->
+        refresh_ip_periodically
+        {:ok, %{ host: host, port: port, ip: ip } }
+      _ -> {:stop, "Invalid host: #{host}"}
+    end
+
   end
+
+  def start_link(host,port) do
+    GenServer.start_link(__MODULE__, {host,port}, name: __MODULE__)
+  end
+
+  def handle_cast({ :send, message }, state) do
+    {:ok, socket} = :gen_udp.open(0)
+    :gen_udp.send(socket, state.ip, state.port, message)
+    :gen_udp.close(socket)
+    { :noreply, state }
+  end
+
+  def handle_call(:stop, _from, state) do
+    {:stop, :normal, :ok, state}
+  end
+
+  def handle_info(:update_ip, state) do
+    { :ok, ip } = :inet.getaddr(String.to_char_list(state.host), :inet)
+    refresh_ip_periodically
+    updated_state = state |> Map.put(:ip, ip)
+    { :noreply, updated_state }
+  end
+
+  def send(message) do
+    :ok = GenServer.cast(__MODULE__, {:send, message})
+  end
+  def stop, do: GenServer.call(__MODULE__, :stop)
+
+  defp refresh_ip_periodically, do: :timer.send_after(@ip_udpdate_interval, self, :update_ip)
 end
---
+++ test/sender_test.exs
@@ -0,0 +1,26 @@
+defmodule TestServer do
+  def start(port, receiver), do: spawn(fn() -> server(port, receiver) end)
+
+  def server(port, receiver) do
+    {:ok, socket} = :gen_udp.open(port, [:binary])
+    once(socket, receiver)
+  end
+  def once(socket, receiver) do
+    receive do
+      {:udp, ^socket, _host, _port, bin} -> send(receiver, {:ok, bin})
+      _ -> :fail
+    end
+  end
+end
+defmodule SenderTest do
+  use ExUnit.Case
+  test "will send a message to the server" do
+    port = 28000
+    host = "localhost"
+    TestServer.start(port,self)
+    LoggerPapertrailBackend.Sender.start_link(host, port)
+    LoggerPapertrailBackend.Sender.send("Hello UDP!")
+    assert_receive {:ok, "Hello UDP!"}, 5000
+    LoggerPapertrailBackend.Sender.stop
+  end
+end
