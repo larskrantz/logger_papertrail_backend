651b7a5e8029d060d275bcc69fe665dc033fe983
Lars Krantz <lars.krantz@alaz.se>
2016-04-18 12:49:39 +0200

    Bump to version 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LoggerPapertrailBackend.Mixfile do
 
   def project do
     [app: :logger_papertrail_backend,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
