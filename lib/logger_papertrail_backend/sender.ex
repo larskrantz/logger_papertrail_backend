2cc840f2a2c92e39c86a6f4cf2131ad0b7db62ce
Lars Krantz <lars.krantz@alaz.se>
2016-10-21 15:26:18 +0200

    Rewrite of sender to use last resolved ip if dns failes. Fixes #4

--- lib/logger_papertrail_backend/sender.ex
+++ lib/logger_papertrail_backend/sender.ex
@@ -1,11 +1,15 @@
 defmodule LoggerPapertrailBackend.Sender do
   use GenServer
 
-  @ip_update_interval_ms 60000
+  defmodule State do
+    defstruct hosts: %{}
+  end
+
+  @ip_update_interval_ms 60_000
   def init(_), do: init()
   def init() do
-    refresh_ips_in_intervalls
-    {:ok, %{}}
+    :timer.send_interval(@ip_update_interval_ms, :update_ip)
+    {:ok, %State{}}
   end
 
   def start_link() do
@@ -13,17 +17,17 @@ defmodule LoggerPapertrailBackend.Sender do
   end
 
    def handle_cast({ :send, message, host, port}, state) when is_integer(port) and is_binary(host) and is_binary(message) do
-    { ip, updated_state } = state |> resolve(host)
+    { ip, new_state } = get_ip_address(state, host)
     {:ok, socket} = :gen_udp.open(0)
     :gen_udp.send(socket, ip, port, message)
     :gen_udp.close(socket)
-    { :noreply, updated_state }
+    { :noreply, new_state }
   end
   def handle_cast({ :send, _message, _host, _port }, state), do: { :noreply, state }
 
   def handle_info(:update_ip, state) do
-    updated_state = state |> Enum.reduce(%{},&refresh_ip/2)
-    refresh_ips_in_intervalls
+    updated_state = refresh_ips(state)
+
     { :noreply, updated_state }
   end
 
@@ -31,25 +35,32 @@ defmodule LoggerPapertrailBackend.Sender do
     :ok = GenServer.cast(__MODULE__, {:send, message, host, port})
   end
 
-  defp refresh_ips_in_intervalls, do: :timer.send_after(@ip_update_interval_ms, self, :update_ip)
-  defp refresh_ip({ host, _ip }, state) do
-    ip = resolve_host(host)
-    state |> Map.put(host, ip)
+  defp refresh_ips(%State{hosts: hosts} = state), do: refresh_ips(Map.keys(hosts), state)
+  defp refresh_ips([], state), do: state
+  defp refresh_ips([ host | tail], state) do
+    { _, updated_state } = resolve_add_host(state, host)
+    refresh_ips(tail, updated_state)
   end
 
-  defp resolve(state, host) do
-    case Map.fetch(state, host) do
+  defp get_ip_address(%State{hosts: hosts} = state, host) do
+    case Map.fetch(hosts, host) do
       {:ok, ip} -> {ip, state}
-      _ -> state |> resolve_add_host(host)
+      _ -> resolve_add_host(state, host)
     end
   end
   defp resolve_host(host) do
-    { :ok, ip } = :inet.getaddr(String.to_char_list(host), :inet)
-    ip
+    case :inet.getaddr(String.to_char_list(host), :inet) do
+      { :ok, ip } -> ip
+      _ -> nil
+    end
   end
   defp resolve_add_host(state, host) do
-    ip = resolve_host(host)
-    new_state = Map.put(state, host, ip)
-    { ip, new_state }
+    case resolve_host(host) do
+      nil ->  last_resolved_ip = Map.get(state.hosts, host)
+              { last_resolved_ip, state }
+      ip ->   new_hosts = Map.put(state.hosts, host, ip)
+              new_state = %State{ state | hosts: new_hosts }
+              { ip, new_state }
+      end
   end
 end
