2983d460964ebfdce89aa6020f814b0742df4272
chris <c.benoit@romy-paris.com>
2017-03-03 13:48:36 +0100

    Elixir 1.4: fix warnings

--- test/message_builder_test.exs
+++ test/message_builder_test.exs
@@ -7,19 +7,19 @@ defmodule LoggerPapertrailBackend.MessageBuilderTest do
 
   test "will create a correct syslog message" do
     procid = Keyword.get(@meta, :module, nil)
-    message = build_message(application, procid)
+    message = build_message(application(), procid)
     assert "<11>Oct  1 12:44:01 upptec_slack_robot UpptecSlack.SlackBot Hello PaperTrail!" == message
   end
 
   test "will trim to long tags so it is still informational" do
     procid = "LoggerPapertrailBackend.MessageBuilder.Sub.VeryLongModuleName"
-    message = build_message(application, procid)
+    message = build_message(application(), procid)
     assert "<11>Oct  1 12:44:01 upptec_slack_robot Sub.VeryLongModuleName Hello PaperTrail!" == message
   end
 
   test "in extrem cases, will only have last module name and trim it" do
     procid = "ABCDEFGHIJKL.MNOPQRSTUVXYZ.ABCDEFGHIJKLMNOPQRS.VeryVeryLongLongLongModuleSentence"
-    message = build_message(application, procid)
+    message = build_message(application(), procid)
     assert "<11>Oct  1 12:44:01 upptec_slack_robot VeryVeryLongLongLongModuleSenten Hello PaperTrail!" == message
   end
 
